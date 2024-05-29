c5e61bfafa34a16dfcaa93dd9ca3de2cc5d09086
Dongwan Kim <kdw6485@gmail.com>
2024-05-29 18:27:00 +0900

    execute command and make a playlist

--- Jellyfin.Plugin.MixFollower/PlaylistRebuildTask.cs
+++ Jellyfin.Plugin.MixFollower/PlaylistRebuildTask.cs
@@ -6,28 +6,41 @@ namespace Jellyfin.Plugin.MixFollower
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Globalization;
     using System.Linq;
     using System.Reflection;
+    using System.Runtime.InteropServices;
     using System.Threading;
     using System.Threading.Tasks;
-    using Emby.Naming.Common;
+    using CliWrap;
+    using CliWrap.Buffered;
+    using Jellyfin.Data.Entities.Libraries;
     using Jellyfin.Data.Enums;
+    using MediaBrowser.Controller.Entities;
+    using MediaBrowser.Controller.Entities.Audio;
     using MediaBrowser.Controller.Library;
     using MediaBrowser.Controller.Playlists;
+    using MediaBrowser.Controller.Providers;
+    using MediaBrowser.Model.Dto;
     using MediaBrowser.Model.Globalization;
     using MediaBrowser.Model.Playlists;
+    using MediaBrowser.Model.Search;
     using MediaBrowser.Model.Tasks;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// Deletes old log files.
     /// </summary>
     public class PlaylistRebuildTask : IScheduledTask, IConfigurableScheduledTask
     {
+        private readonly Guid firstAdminId;
         private readonly ILibraryManager libraryManager;
         private readonly IPlaylistManager playlistManager;
         private readonly IUserManager userManager;
+        private readonly ISearchEngine searchEngine;
 
         private readonly ILocalizationManager localization;
         private readonly ILogger<PlaylistRebuildTask> logger;
@@ -40,13 +53,16 @@ namespace Jellyfin.Plugin.MixFollower
         /// <param name="playlistManager">The playlist manager.</param>
         /// <param name="logger">The  logger.</param>
         /// <param name="localization">The localization manager.</param>
-        public PlaylistRebuildTask(ILibraryManager libraryManager, IUserManager userManager, IPlaylistManager playlistManager, ILogger<PlaylistRebuildTask> logger, ILocalizationManager localization)
+        public PlaylistRebuildTask(ILibraryManager libraryManager, ISearchEngine searchEngine, IUserManager userManager, IPlaylistManager playlistManager, ILogger<PlaylistRebuildTask> logger, ILocalizationManager localization)
         {
             this.userManager = userManager;
             this.playlistManager = playlistManager;
             this.libraryManager = libraryManager;
             this.logger = logger;
             this.localization = localization;
+            this.searchEngine = searchEngine;
+            this.firstAdminId = this.userManager.Users
+                .First(i => i.HasPermission(PermissionKind.IsAdministrator)).Id;
             this.logger.LogInformation("PlaylistRebuildTask constructed");
         }
 
@@ -86,42 +102,130 @@ namespace Jellyfin.Plugin.MixFollower
             };
         }
 
-        /// <inheritdoc />
-        public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+        private void DeletePlaylist(string playlist_name)
         {
-            this.logger.LogInformation("ExecuteAsync");
-
-            var apis_download = Plugin.Instance.Configuration.ApisDownload;
-            var commands_to_fetch = Plugin.Instance.Configuration.CommandsToFetch;
-
-            this.logger.LogInformation("commands_to_fetch : {0}", commands_to_fetch[0]);
-
-            const string PLAYLIST_NAME = "TOAST";
-            var firstAdminId = this.userManager.Users
-                .First(i => i.HasPermission(PermissionKind.IsAdministrator)).Id;
-            var playlists = this.playlistManager.GetPlaylists(firstAdminId);
-            var item_ids = Array.Empty<Guid>();
-            foreach (var playlist in playlists)
+            var playlists = this.playlistManager.GetPlaylists(this.firstAdminId);
+            var playlist = playlists.FirstOrDefault(playlist => playlist.Name == playlist_name);
+            if (playlist is null)
             {
-                this.logger.LogInformation("retrieve playlist : {Retri}", playlist.Name);
-                if (playlist.Name == PLAYLIST_NAME)
-                {
-                    this.libraryManager.DeleteItem(playlist, new DeleteOptions { DeleteFileLocation = true }, true);
-                    this.logger.LogInformation("matched and deleted ");
-                }
+                this.logger.LogInformation("there is no playlist {Name}", playlist_name);
+                return;
             }
 
-            var result = await this.playlistManager.CreatePlaylist(new PlaylistCreationRequest
+            this.libraryManager.DeleteItem(playlist, new DeleteOptions { DeleteFileLocation = true }, true);
+            this.logger.LogInformation("matched and deleted ");
+        }
+
+        private async void CreatePlaylistFromFetchCommand(Guid user, string command)
+        {
+            var result = await Cli.Wrap(command).ExecuteBufferedAsync();
+            if (!result.IsSuccess)
             {
-                Name = PLAYLIST_NAME,
-                ItemIdList = item_ids,
-                UserId = firstAdminId,
+                throw new InvalidOperationException($"{command} failed! error code: {result.ExitCode}");
+            }
+
+            JObject obj = JObject.Parse(result.StandardOutput);
+            string playlist_name = obj["name"] !.ToString();
+            var songs = obj["songs"];
+            var list_items =[];
+            foreach (var song in songs)
+            {
+                var title = song["title"] !.ToString();
+                var artist = song["artist"] !.ToString();
+                var item = this.GetMostMatchedSong(title, artist);
+                if (item.Id == Guid.Empty)
+                {
+                    this.logger.LogInformation("song {Title} by {Artist} not found in library", title, artist);
+                    /*
+                    item = DownloadMusic();
+                    if(item == Guid.Empty)
+                    {
+                        throw new InvalidOperationException("");
+                    }*/
+                }
+
+                list_items.Add(item);
+            }
+
+            this.DeletePlaylist(playlist_name);
+
+            var playlist = await this.playlistManager.CreatePlaylist(new PlaylistCreationRequest
+            {
+                Name = playlist_name,
+                ItemIdList = list_items,
+                UserId = user,
                 MediaType = Data.Enums.MediaType.Audio,
 
                 // Users = [],
                 Public = true,
             }).ConfigureAwait(false);
-            this.logger.LogInformation("playlist created {Id}", result.Id);
+            this.logger.LogInformation("playlist created {Id}", playlist.Id);
+        }
+
+        private Audio ConvertSearchHintInfoToAudio(SearchHintInfo hintInfo)
+        {
+            var item = hintInfo.Item;
+            switch (item)
+            {
+                case Audio song:
+                    return song;
+                default:
+                    throw new ArgumentException("base item is not a type of audio");
+            }
+        }
+
+        private Audio GetMostMatchedSong(string title, string artist)
+        {
+            var hints = this.searchEngine.GetSearchHints(new SearchQuery()
+            {
+                MediaTypes =[MediaType.Audio],
+                SearchTerm = title,
+            });
+            return hints.Items
+            .Select(this.ConvertSearchHintInfoToAudio)
+            .Where(song =>
+                song.Artists.Any(song_artist => song_artist.Contains(artist) || artist.Contains(song_artist)))
+            .FirstOrDefault();
+        }
+
+        private async Task<bool> DownloadMusicFromSource(string api, string title, string artist)
+        {
+            return true;
+        }
+
+        private async Task<Audio> DownloadMusic(string title, string artist)
+        {
+            var apis_download = Plugin.Instance.Configuration.ApisDownload;
+            foreach (var api in apis_download)
+            {
+                try
+                {
+                    var success = this.DownloadMusicFromSource(api, title, artist);
+
+                    return this.GetMostMatchedSong(title, artist);
+                }
+                catch (Exception e)
+                {
+                    this.logger.LogInformation("download from {Source} failed  {Msg}", api, e.Message);
+                }
+            }
+
+            this.logger.LogInformation("tried all wonload source but all failed.");
+            return new Audio();
+        }
+
+        /// <inheritdoc />
+        public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("ExecuteAsync");
+
+            var commands_to_fetch = Plugin.Instance.Configuration.CommandsToFetch;
+
+            this.logger.LogInformation("commands_to_fetch : {0}", commands_to_fetch[0]);
+
+            var item_ids = Array.Empty<Guid>();
+
+            commands_to_fetch.ForEach(command => this.CreatePlaylistFromFetchCommand(this.firstAdminId, command));
         }
     }
 }
