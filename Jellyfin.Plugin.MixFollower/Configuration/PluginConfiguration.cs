4c104143a1c34f61e205ebbd4a12a4d39e87c6e5
Dongwan Kim <kdw6485@gmail.com>
2024-05-29 21:18:31 +0900

    now time to configpage

--- Jellyfin.Plugin.MixFollower/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.MixFollower/Configuration/PluginConfiguration.cs
@@ -33,8 +33,8 @@ public class PluginConfiguration : BasePluginConfiguration
     public PluginConfiguration()
     {
         // set default options here
-        this.ApisDownload =[string.Empty];
-        this.CommandsToFetch =["on_testing"];
+        this.ApisDownload = new List<string>();
+        this.CommandsToFetch = new List<string>();
     }
 
     /// <summary>
